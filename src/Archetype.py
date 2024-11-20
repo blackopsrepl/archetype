f49768d2b9e312353ab873bef557293bf12c8a96
Vittorio Distefano <info@vdistefano.studio>
2024-11-20 13:07:49 +0100

    refactor(archetype): Refactor setup_template and setup_chain methods
    Refactored the `setup_template` method in the `Archetype` class to use a dictionary instead of multiple if-else statements for template selection based on archetype and language. Also refactored the `setup_chain` method to use lambda functions and a dictionary for chain setup based on archetype.
    
    - Replaced multiple if-else statements with a dictionary lookup for template selection.
    - Replaced separate if conditions with lambda functions and a dictionary lookup for chain setup based on archetype.
    
    Note: The removed import statements are not listed as significant changes since they don't affect the functionality of the code.

--- src/Archetype.py
+++ src/Archetype.py
@@ -1,22 +1,16 @@
-import os, string, random
 from datetime import datetime
 import streamlit as st
 
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import StrOutputParser
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
 
-from langchain.document_loaders import PyPDFLoader
 from langchain.memory import ConversationBufferMemory, ConversationEntityMemory
 from langchain.memory.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
 from langchain.utilities import WikipediaAPIWrapper
 from langchain.chains import LLMChain, ConversationChain, RetrievalQA
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
-from langchain.vectorstores import Weaviate
 from operator import itemgetter
-import weaviate
 
 import utils
 from utils import StreamHandler
@@ -49,14 +43,13 @@ def archetype_factory(archetype):
         #####################
 
         def setup_template(self):
-            if archetype == "DIALOGOS" and st.session_state["LANGUAGE"] == "en":
-                return dialogos_eng
-            if archetype == "DIALOGOS" and st.session_state["LANGUAGE"] == "it":
-                return dialogos_ita
-            if archetype == "THESIS" and st.session_state["LANGUAGE"] == "en":
-                return thesis_eng
-            if archetype == "THESIS" and st.session_state["LANGUAGE"] == "it":
-                return thesis_ita
+            templates = {
+                ("DIALOGOS", "en"): dialogos_eng,
+                ("DIALOGOS", "it"): dialogos_ita,
+                ("THESIS", "en"): thesis_eng,
+                ("THESIS", "it"): thesis_ita,
+            }
+            return templates.get((archetype, st.session_state["LANGUAGE"]))
 
         if archetype == "THESIS":
 
@@ -76,108 +69,79 @@ def archetype_factory(archetype):
                     timeline = f"\n\npianifica una timeline lineare fattibile per una tesi di maturità su {topic} con questo outline:\n{outline}.\nla tesi deve essere considerata come il compito principale e deve essere scomposta in sotto-compiti.\ni sotto-compiti devono essere organizzati in modo fattibile nella timeline.\nUsa {subtopics} come sotto-compiti.\nApplica questo executive loop aggiuntivo:\n{executive_loop}\nFornisci una timeline ordinata di sotto-compiti per la tesi. Data di inizio: {start_date}. Scadenza: {end_date}.\nFormato: elenco puntato markdown.\n\n"
                 return timeline
 
-        def setup_chain(self, docsearch=None):
+        def setup_chain(self):
+            archetype_chains = {
+                "DIALOGOS": lambda: self._setup_dialogos(),
+                "THESIS": lambda: self._setup_thesis(),
+            }
 
-            if archetype == "DIALOGOS":
-                t = self.setup_template()
-                llm = ChatOpenAI(
-                    model_name=self.openai_model, temperature=0, streaming=True
-                )
-                if "entity_memory" not in st.session_state:
-                    st.session_state["entity_memory"] = ConversationEntityMemory(
-                        llm=llm, k=50
-                    )
-                first_chain = LLMChain(
-                    llm=llm,
-                    verbose=True,
-                    prompt=t["dialogos_template"],
-                    memory=st.session_state.entity_memory,
-                )
-                second_chain = ConversationChain(
-                    llm=llm,
-                    prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-                    verbose=True,
-                    memory=st.session_state.entity_memory,
-                )
+            return archetype_chains.get(archetype, lambda: None)()
 
-            if archetype == "THESIS":
-                t = self.setup_template()
-                executive_loop = t["executive_loop"]
-                subtopics_template = t["subtopics_template"]
-                axes_template = t["axes_template"]
-                outline_template = t["outline_template"]
-                timeline_template = t["timeline_template"]
-                analysis_template = t["analysis_template"]
+        def _create_chain(self, template, llm, memory):
+            return (
+                RunnablePassthrough.assign(
+                    memory=RunnableLambda(memory.load_memory_variables)
+                    | itemgetter("history")
+                )
+                | template
+                | llm
+                | StrOutputParser()
+            )
 
-                ### LLM ###
-                llm_dry = OpenAI(temperature=0.30, streaming=True)
-                llm_verbose = ChatOpenAI(
-                    model_name=self.openai_model, temperature=0.45, streaming=True
+        def _setup_dialogos(self):
+            t = self.setup_template()
+            llm = ChatOpenAI(
+                model_name=self.openai_model, temperature=0, streaming=True
+            )
+            if "entity_memory" not in st.session_state:
+                st.session_state["entity_memory"] = ConversationEntityMemory(
+                    llm=llm, k=50
                 )
+            first_chain = LLMChain(
+                llm=llm,
+                verbose=True,
+                prompt=t["dialogos_template"],
+                memory=st.session_state.entity_memory,
+            )
+            second_chain = ConversationChain(
+                llm=llm,
+                prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+                verbose=True,
+                memory=st.session_state.entity_memory,
+            )
+            return first_chain, second_chain, t
 
-                ### MEMORY ###
-                memory = ConversationBufferMemory(return_messages=True)
-                memory.load_memory_variables({})
+        def _setup_thesis(self):
+            t = self.setup_template()
+            executive_loop = t["executive_loop"]  # called within prompt_templates.py
+            subtopics_template = t["subtopics_template"]
+            axes_template = t["axes_template"]
+            outline_template = t["outline_template"]
+            timeline_template = t["timeline_template"]
+            analysis_template = t["analysis_template"]
 
-                ### CHAINS ###
-                subtopics_chain = (
-                    RunnablePassthrough.assign(
-                        memory=RunnableLambda(memory.load_memory_variables)
-                        | itemgetter("history")
-                    )
-                    | subtopics_template
-                    | llm_dry
-                    | StrOutputParser()
-                )
-                axes_chain = (
-                    RunnablePassthrough.assign(
-                        memory=RunnableLambda(memory.load_memory_variables)
-                        | itemgetter("history")
-                    )
-                    | axes_template
-                    | llm_dry
-                    | StrOutputParser()
-                )
-                outline_chain = (
-                    RunnablePassthrough.assign(
-                        memory=RunnableLambda(memory.load_memory_variables)
-                        | itemgetter("history")
-                    )
-                    | outline_template
-                    | llm_verbose
-                    | StrOutputParser()
-                )
-                timeline_chain = (
-                    RunnablePassthrough.assign(
-                        memory=RunnableLambda(memory.load_memory_variables)
-                        | itemgetter("history")
-                    )
-                    | timeline_template
-                    | llm_dry
-                    | StrOutputParser()
-                )
-                user_query = ""
-                analysis_chain = (
-                    RunnablePassthrough.assign(
-                        memory=RunnableLambda(memory.load_memory_variables)
-                        | itemgetter("history")
-                    )
-                    | analysis_template
-                    | llm_verbose
-                    | StrOutputParser()
-                )
+            llm_dry = OpenAI(temperature=0.30, streaming=True)
+            llm_verbose = ChatOpenAI(
+                model_name=self.openai_model, temperature=0.45, streaming=True
+            )
 
-            if archetype == "DIALOGOS":
-                return first_chain, second_chain, t
-            if archetype == "THESIS":
-                return (
-                    subtopics_chain,
-                    axes_chain,
-                    outline_chain,
-                    timeline_chain,
-                    analysis_chain,
-                    t,
-                )
+            memory = ConversationBufferMemory(return_messages=True)
+            memory.load_memory_variables({})
+
+            subtopics_chain = self._create_chain(subtopics_template, llm_dry, memory)
+            axes_chain = self._create_chain(axes_template, llm_dry, memory)
+            outline_chain = self._create_chain(outline_template, llm_verbose, memory)
+            timeline_chain = self._create_chain(timeline_template, llm_dry, memory)
+            analysis_chain = self._create_chain(analysis_template, llm_verbose, memory)
+
+            return (
+                subtopics_chain,
+                axes_chain,
+                outline_chain,
+                timeline_chain,
+                analysis_chain,
+                t,
+            )
 
         @utils.enable_chat_history
         def main(self):
